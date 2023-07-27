0eb0df47a0acf0a13ac3ec0156392b52c00bbbbf
rachithans <Rachit.hans@dal.ca>
2023-07-26 23:43:03 -0400

    merging dev branch

--- server/routes/Logininfo.mjs
+++ server/routes/Logininfo.mjs
@@ -1,10 +1,35 @@
+// Author: Bhavya Jain
 import express from "express";
 import db from "../db/conn.mjs";
 import * as bcrypt from 'bcrypt';
 import jwt from "jsonwebtoken";
+import { ObjectId } from 'mongodb';
 
 const router = express.Router();
 const saltRounds = 10; // Number of rounds to use for hashing
+
+// This section will help you get user details by id.
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const objectId = new ObjectId(id);
+    const collection = await db.collection("LoginInfo");
+    const user = await collection.findOne({ _id: objectId });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Removing sensitive information
+    const { password, ...userData } = user;
+
+    res.json(userData);
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 // This section will help you create a new record.
 router.post("/register", async (req, res) => {
@@ -44,15 +69,15 @@ router.post("/register", async (req, res) => {
 
 // This section will help you update a record by id.
 router.post("/loginUser", async (req, res) => {
-  const {username, password} = req.body;
-  if (!username || !password) {
-    return res.status(400).send("Please provide username and password");
+  const {email, password} = req.body;
+  if (!email || !password) {
+    return res.status(400).send("Please provide email and password");
   }
   else{
     try{
-      const user = await db.collection("LoginInfo").findOne({username});
+      const user = await db.collection("LoginInfo").findOne({email});
       if(!user) {
-        return res.status(400).send("Invalid username");
+        return res.status(400).send("Invalid email");
       }
       if(await bcrypt.compare(password, user.password)) {
         const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn: "1d"});
@@ -65,6 +90,30 @@ router.post("/loginUser", async (req, res) => {
     }
   }
 });
+
+// This section will help you update your password
+router.post("/forgotPassword", async (req, res) => {
+  try{
+    // Check if the user already exists
+    let collection = await db.collection("LoginInfo");
+    const user = await collection.findOne({email: req.body.email});
+    if(!user) {
+      return res.status(400).send("User not found");
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    // Update the user's password
+    let result = await collection.updateOne({email: req.body.email}, {$set: {password: hashedPassword}});
+    res.send(result).status(204);
+  }
+  catch (error){
+    console.log(error);
+    res.send("Internal server error").status(500);
+  }
+ 
+});
+
 
 
 export default router;
