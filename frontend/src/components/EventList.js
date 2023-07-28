24bd6c6b45399e61de8f4d44aaafca985c5de39f
Sahil Sorathiya <sahil.sorathiya@dal.ca>
2023-07-28 01:16:32 -0300

    modal box added

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -61,7 +61,7 @@ export default function EventList({ userId }) {
         }
       );
       console.log("Booking Successful!!");
-      getRecords();
+      
     } catch (error) {
       console.log("Booking Failed!!", error);
     }
@@ -114,9 +114,29 @@ export default function EventList({ userId }) {
                 onClick={() =>
                   handleBooking(record.eventId, count[record.eventId])
                 }
+                data-bs-toggle="modal" data-bs-target="#exampleModal"
               >
                 Book Now
               </button>
+
+              <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                      ...
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={getRecords}>Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
             </div>
           </div>
         </div>
